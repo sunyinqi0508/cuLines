e71c9a7971973793a99b4e2de3d6f932697ca3fd
chengluyu <chengluyu@live.cn>
2018-03-24 21:02:03 +0800

    fix style

--- cuLines/LSH.cpp
+++ cuLines/LSH.cpp
@@ -220,30 +220,30 @@ public:
 			}
 		unordered_map<int, int>::iterator it = res.begin();
 
-        if (from_distinct_lines) {
-            auto seg_of_lines = new int[n_streamlines];
-            std::fill(seg_of_lines, seg_of_lines + n_streamlines, -1);
-            while (it != res.end()) {
-                auto line_idx = segments[it->second].line;
-                if (seg_of_lines[line_idx] == -1)
-                    seg_of_lines[line_idx] = it->second;
-                else {
-                    auto old_dist = (segments[seg_of_lines[line_idx]].centroid - point).length();
-                    auto new_dist = (segments[it->second].centroid - point).length();
-                    if (new_dist < old_dist)
-                        seg_of_lines[line_idx] = it->second;
-                }
-            }
-            for (int i = 0; i < n_streamlines; i++)
-                if (seg_of_lines[i] >= 0)
-                    results.emplace_back(seg_of_lines[i]);
-            delete[] seg_of_lines;
-        } else {
-            while (it != res.end()) {
-                results.push_back(it->second);
-                it++; 
-            }
-        }
+		if (from_distinct_lines) {
+			auto seg_of_lines = new int[n_streamlines];
+			std::fill(seg_of_lines, seg_of_lines + n_streamlines, -1);
+			while (it != res.end()) {
+				auto line_idx = segments[it->second].line;
+				if (seg_of_lines[line_idx] == -1)
+					seg_of_lines[line_idx] = it->second;
+				else {
+					auto old_dist = (segments[seg_of_lines[line_idx]].centroid - point).length();
+					auto new_dist = (segments[it->second].centroid - point).length();
+					if (new_dist < old_dist)
+						seg_of_lines[line_idx] = it->second;
+				}
+			}
+			for (int i = 0; i < n_streamlines; i++)
+				if (seg_of_lines[i] >= 0)
+					results.emplace_back(seg_of_lines[i]);
+			delete[] seg_of_lines;
+		} else {
+			while (it != res.end()) {
+				results.push_back(it->second);
+				it++; 
+			}
+		}
 	}
 
 
@@ -251,115 +251,115 @@ public:
 
 template <typename S, typename T>
 bool compareFirstOnly(const pair<S, T> &lhs, const pair<S, T> &rhs) {
-    return lhs.first < rhs.first;
+	return lhs.first < rhs.first;
 }
 
 template <typename S, typename T>
 void sortByFirst(vector<pair<S, T>> &c) {
-    sort(begin(c), end(c), compareFirstOnly<S, T>);
+	sort(begin(c), end(c), compareFirstOnly<S, T>);
 }
 
 inline int getGlobalIndex(int line_idx, int pt_idx) {
-    return &f_streamlines[line_idx][pt_idx] - &f_streamlines[0][0];
+	return &f_streamlines[line_idx][pt_idx] - &f_streamlines[0][0];
 }
 
 struct Point {
-    int line_idx, point_idx, global_idx;
+	int line_idx, point_idx, global_idx;
 
-    Point(int line_index, int point_index) :
-        line_idx{ line_index },
-        point_idx{ point_idx },
-        global_idx{ getGlobalIndex(line_index, point_index) }
-    {}
+	Point(int line_index, int point_index) :
+		line_idx{ line_index },
+		point_idx{ point_idx },
+		global_idx{ getGlobalIndex(line_index, point_index) }
+	{}
 };
 
 vector<pair<float, int>> queryGroundTruth(Vector3 pt) {
-    vector<pair<float, int>> result;
-    // for each lines
-    for (int i = 0; i < n_streamlines; i++) {
-        auto nearest_dist = numeric_limits<float>::infinity();
-        Vector *nearest_vec = nullptr;
-        auto n = static_cast<int>(streamlines[i].size());
-        for (int j = 0; j < n; j++) {
-            auto dist = f_streamlines[i][j].distance(pt);
-            if (dist < nearest_dist) {
-                nearest_vec = &f_streamlines[i][j];
-                nearest_dist = dist;
-            }
-        }
-        if (nearest_vec) {
-            auto pt_glob_idx = nearest_vec - &f_streamlines[0][0];
-            result.emplace_back(nearest_dist, pt_glob_idx);
-        }
-    }
-    sort(begin(result), end(result), compareFirstOnly<float, int>);
-    return result;
+	vector<pair<float, int>> result;
+	// for each lines
+	for (int i = 0; i < n_streamlines; i++) {
+		auto nearest_dist = numeric_limits<float>::infinity();
+		Vector *nearest_vec = nullptr;
+		auto n = static_cast<int>(streamlines[i].size());
+		for (int j = 0; j < n; j++) {
+			auto dist = f_streamlines[i][j].distance(pt);
+			if (dist < nearest_dist) {
+				nearest_vec = &f_streamlines[i][j];
+				nearest_dist = dist;
+			}
+		}
+		if (nearest_vec) {
+			auto pt_glob_idx = nearest_vec - &f_streamlines[0][0];
+			result.emplace_back(nearest_dist, pt_glob_idx);
+		}
+	}
+	sort(begin(result), end(result), compareFirstOnly<float, int>);
+	return result;
 }
 
 void segmentIndexToLineIndex(vector<int> &indexes) {
-    for (auto &idx : indexes)
-        idx = static_cast<int>(segments[idx].line);
+	for (auto &idx : indexes)
+		idx = static_cast<int>(segments[idx].line);
 }
 
 vector<int> querySegments(vector<HashTable> &hts, Vector3 pt) {
-    // do first level query
-    vector<int> query_result;
-    auto seg_of_lines = new int[n_streamlines];
-    std::fill(seg_of_lines, seg_of_lines + n_streamlines, -1);
-    // merge query result from all hash tables
-    for (auto &ht : hts) {
-        query_result.clear();
-        ht.Query(query_result, pt, false);
-        for (auto seg_idx : query_result) {
-            // line index of this segment
-            auto line_idx = segments[seg_idx].line;
-            // if this line has not been recorded
-            if (seg_of_lines[line_idx] == -1)
-                seg_of_lines[line_idx] = seg_idx;
-            else {
-                // test if nearer
-                auto old_dist = (segments[seg_of_lines[line_idx]].centroid - pt).length();
-                auto new_dist = (segments[seg_idx].centroid - pt).length();
-                if (new_dist < old_dist)
-                    seg_of_lines[line_idx] = seg_idx;
-            }
-        }
-    }
-    query_result.clear();
-    for (int i = 0; i < n_streamlines; i++)
-        if (seg_of_lines[i] >= 0)
-            query_result.emplace_back(i);
-    delete[] seg_of_lines;
-    return query_result;
+	// do first level query
+	vector<int> query_result;
+	auto seg_of_lines = new int[n_streamlines];
+	std::fill(seg_of_lines, seg_of_lines + n_streamlines, -1);
+	// merge query result from all hash tables
+	for (auto &ht : hts) {
+		query_result.clear();
+		ht.Query(query_result, pt, false);
+		for (auto seg_idx : query_result) {
+			// line index of this segment
+			auto line_idx = segments[seg_idx].line;
+			// if this line has not been recorded
+			if (seg_of_lines[line_idx] == -1)
+				seg_of_lines[line_idx] = seg_idx;
+			else {
+				// test if nearer
+				auto old_dist = (segments[seg_of_lines[line_idx]].centroid - pt).length();
+				auto new_dist = (segments[seg_idx].centroid - pt).length();
+				if (new_dist < old_dist)
+					seg_of_lines[line_idx] = seg_idx;
+			}
+		}
+	}
+	query_result.clear();
+	for (int i = 0; i < n_streamlines; i++)
+		if (seg_of_lines[i] >= 0)
+			query_result.emplace_back(i);
+	delete[] seg_of_lines;
+	return query_result;
 }
 
 vector<pair<float, int>> queryANN(vector<HashTable> &hts, Vector3 pt) {
-    // do first level query
-    auto query_result = querySegments(hts, pt);
-    // do second level query
-    vector<pair<float, int>> result;
-    for (auto seg_idx : query_result) {
-        auto nearest = second_level[seg_idx].nearest(pt);
-        auto nearest_glob_idx = getGlobalIndex(segments[seg_idx].line, nearest);
-        auto nearest_dist = f_streamlines[0][nearest_glob_idx].distance(pt);
-        result.emplace_back(nearest_dist, nearest_glob_idx);
-    }
-    sort(begin(result), end(result), compareFirstOnly<float, int>);
-    return result;
+	// do first level query
+	auto query_result = querySegments(hts, pt);
+	// do second level query
+	vector<pair<float, int>> result;
+	for (auto seg_idx : query_result) {
+		auto nearest = second_level[seg_idx].nearest(pt);
+		auto nearest_glob_idx = getGlobalIndex(segments[seg_idx].line, nearest);
+		auto nearest_dist = f_streamlines[0][nearest_glob_idx].distance(pt);
+		result.emplace_back(nearest_dist, nearest_glob_idx);
+	}
+	sort(begin(result), end(result), compareFirstOnly<float, int>);
+	return result;
 }
 
 float evaluateError(
-    const vector<pair<float, int>> &groundtruth,
-    const vector<pair<float, int>> &ann_result
+	const vector<pair<float, int>> &groundtruth,
+	const vector<pair<float, int>> &ann_result
 ) {
-    auto n = min(groundtruth.size(), ann_result.size());
-    auto errorSum = 0.f;
-    for (size_t i = 0; i < n; i++) {
-        auto dist_gt = groundtruth[i].first;
-        auto dist_ann = ann_result[i].first;
-        errorSum += fabsf(dist_gt - dist_ann);
-    }
-    return errorSum / static_cast<float>(n);
+	auto n = min(groundtruth.size(), ann_result.size());
+	auto errorSum = 0.f;
+	for (size_t i = 0; i < n; i++) {
+		auto dist_gt = groundtruth[i].first;
+		auto dist_ann = ann_result[i].first;
+		errorSum += fabsf(dist_gt - dist_ann);
+	}
+	return errorSum / static_cast<float>(n);
 }
 
 void arrangement(int n_buckets, int n_tuple, int* buckets) {
@@ -371,101 +371,102 @@ vector<HashTable> hashtables;
 // do application on critical points query
 
 enum class CriticalPointType : int {
-    RepelNode = 0x0,
-    RepelFocus = 0x4,
-    RepelNodeSaddle = 0x1,
-    RepelFocusSaddle = 0x5,
-    AttractNodeSaddle = 0x3,
-    AttractFocusSaddle = 0x7,
-    AttractNode = 0x2,
-    AttractFocus = 0x6
+	RepelNode = 0x0,
+	RepelFocus = 0x4,
+	RepelNodeSaddle = 0x1,
+	RepelFocusSaddle = 0x5,
+	AttractNodeSaddle = 0x3,
+	AttractFocusSaddle = 0x7,
+	AttractNode = 0x2,
+	AttractFocus = 0x6
 };
 
 struct CriticalPoint : public Vector3 {
-    CriticalPointType type;
-    float scale;
+	CriticalPointType type;
+	float scale;
 };
 
 template <typename T>
 inline void readRaw(std::istream &in, T &x) {
-    in.read(reinterpret_cast<char*>(&x), sizeof(T));
+	in.read(reinterpret_cast<char*>(&x), sizeof(T));
 }
 
 std::vector<CriticalPoint> loadCriticalPoints(const char *filename) {
-    std::ifstream inputFile{ filename, std::ifstream::binary };
-    uint32_t n_points;
-    readRaw(inputFile, n_points);
-    std::vector<CriticalPoint> pts{ n_points };
-    for (int i = 0; i < n_points; i++)
-        readRaw<Vector3>(inputFile, pts[i]);
-    for (int i = 0; i < n_points; i++)
-        readRaw(inputFile, pts[i].type);
-    for (int i = 0; i < n_points; i++)
-        readRaw(inputFile, pts[i].scale);
-    return pts;
+	std::ifstream inputFile{ filename, std::ifstream::binary };
+	uint32_t n_points;
+	readRaw(inputFile, n_points);
+	std::vector<CriticalPoint> pts{ n_points };
+	for (int i = 0; i < n_points; i++)
+		readRaw<Vector3>(inputFile, pts[i]);
+	for (int i = 0; i < n_points; i++)
+		readRaw(inputFile, pts[i].type);
+	for (int i = 0; i < n_points; i++)
+		readRaw(inputFile, pts[i].scale);
+	return pts;
 }
 
 vector<int> queryCriticalPoints(const vector<CriticalPoint> &query_points, vector<HashTable> &hashTables) {
-    std::vector<bool> mark;
-    mark.assign(n_streamlines, false);
-    int total_marked = 0;
-    for (auto &p : query_points) {
-        auto query_result = querySegments(hashtables, p);
-        segmentIndexToLineIndex(query_result);
-        for (auto line_idx : query_result) {
-            if (!mark[line_idx]) {
-                mark[line_idx] = true;
-                total_marked++;
-            }
-        }
-    }
-    std::vector<int> res;
-    res.reserve(total_marked);
-    for (int i = 0; i < n_streamlines; i++)
-        if (mark[i])
-            res.emplace_back(i);
-    return res;
+	std::vector<bool> mark;
+	mark.assign(n_streamlines, false);
+	int total_marked = 0;
+	for (auto &p : query_points) {
+		auto query_result = querySegments(hashtables, p);
+		segmentIndexToLineIndex(query_result);
+		for (auto line_idx : query_result) {
+			if (!mark[line_idx]) {
+				mark[line_idx] = true;
+				total_marked++;
+			}
+		}
+	}
+	std::vector<int> res;
+	res.reserve(total_marked);
+	for (int i = 0; i < n_streamlines; i++)
+		if (mark[i])
+			res.emplace_back(i);
+	return res;
 }
 
 // do spectral clustering
 
 float computeSimilarity(int p, int q, int windowRadius = 20) {
-    // the middle point index of segment p and segment q
-    auto p_middle = (segments[p].end + segments[p].begin) / 2;
-    auto q_middle = (segments[q].end + segments[q].begin) / 2;
-    // range of the window
-    Range<int> window{ -windowRadius, +windowRadius };
-    auto p_range = window + p_middle;
-    auto q_range = window + q_middle;
-    // clamp the window in the boundary of segment
-    p_range.clampBy({ segments[p].begin, segments[p].end - 1 });
-    q_range.clampBy({ segments[q].begin, segments[q].end - 1 });
-    // find the common part
-    auto common = (p_range -= p_middle).intersect(q_range -= q_middle);
-    auto sum = 0.f;
-    auto p_curve = f_streamlines[segments[p].line];
-    auto q_curve = f_streamlines[segments[q].line];
-    for (int i = common.left; i <= common.right; i++) {
-        auto d = p_curve[p_middle + i].distance(q_curve[q_middle + i]);
-        sum += d * d;
-    }
-    return sum / static_cast<float>(common.length());
+	// the middle point index of segment p and segment q
+	auto p_middle = (segments[p].end + segments[p].begin) / 2;
+	auto q_middle = (segments[q].end + segments[q].begin) / 2;
+	// range of the window
+	Range<int> window{ -windowRadius, +windowRadius };
+	auto p_range = window + p_middle;
+	auto q_range = window + q_middle;
+	// clamp the window in the boundary of segment
+	p_range.clampBy({ segments[p].begin, segments[p].end - 1 });
+	q_range.clampBy({ segments[q].begin, segments[q].end - 1 });
+	// find the common part
+	auto common = (p_range -= p_middle).intersect(q_range -= q_middle);
+	auto sum = 0.f;
+	auto p_curve = f_streamlines[segments[p].line];
+	auto q_curve = f_streamlines[segments[q].line];
+	for (int i = common.left; i <= common.right; i++) {
+		auto d = p_curve[p_middle + i].distance(q_curve[q_middle + i]);
+		sum += d * d;
+	}
+	return sum / static_cast<float>(common.length());
 }
 
 void dumpAffinityMatrix(ostream &out, vector<HashTable> &hashTables) {
-    // foreach centroid of segments
-    out << segments.size() << '\n';
-    for (size_t i = 0; i < segments.size(); i++) {
-        auto ann_segments = querySegments(hashTables, segments[i].centroid);
-        for (auto seg_idx : ann_segments) {
-            out << i << ' '
-                << seg_idx << ' '
-                << computeSimilarity(i, seg_idx) << '\n';
-        }
-    }
+	// foreach centroid of segments
+	out << segments.size() << '\n';
+	for (size_t i = 0; i < segments.size(); i++) {
+		auto ann_segments = querySegments(hashTables, segments[i].centroid);
+		for (auto seg_idx : ann_segments) {
+			out << i << ' '
+				<< seg_idx << ' '
+				<< computeSimilarity(i, seg_idx) << '\n';
+		}
+	}
 }
 
 float *alpha;
+
 int lsh_benchmark(const char* filename) {
 	LoadWaveFrontObject(filename);
 	//FileIO::normalize();
@@ -474,10 +475,12 @@ int lsh_benchmark(const char* filename) {
 	std::fill(alpha, alpha + n_points, 1.f);
   	decomposeByCurvature(2*M_PI, 1000.f);
 	initializeSecondLevel();
-    cout << n_points << " points, " << n_streamlines << " stream lines and " << segments.size() << " segments\n";
-	pair<vector<LshFunc>*, int> funcs = LshFunc::create(funcpool_size, segments.data(), segments.size(), 5);
+	cout << n_points << " points, " << n_streamlines << " stream lines and " << segments.size() << " segments\n";
 
-	//stocastic table construction
+	// hash function pool
+	auto funcs = LshFunc::create(funcpool_size, segments.data(), segments.size(), 5);
+
+	// create the hash tables stocastically
 	uniform_int_distribution<int> uni_dist{ 0, funcpool_size - 1};
 	for (int l = 0; l < L; l++) {
 		
